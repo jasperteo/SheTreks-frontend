30d97cc84055e5e240506d24395002a159a41630
jasperteo <jaspertzj@outlook.sg>
2024-02-26 03:16:50 +0800

    set up supbase, tailwind, diasyui, clerk, tanstack query

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <img
-        width="200"
-        height="200"
-        src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/200/external-tourist-tropical-flaticons-lineal-color-flat-icons.png"
-        alt="external-tourist-tropical-flaticons-lineal-color-flat-icons"
-      />
-      <h1 style={{ fontSize: "4em" }}>
+      <div className="flex items-center justify-center">
+        <img
+          width="200"
+          height="200"
+          src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/200/external-tourist-tropical-flaticons-lineal-color-flat-icons.png"
+          alt="external-tourist-tropical-flaticons-lineal-color-flat-icons"
+        />
+      </div>
+      <h1 className="font-sans text-6xl font-bold">
         SheTreks Illusion 1234567890
         <iconify-icon inline icon="line-md:phone-call-twotone-loop" />
       </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <button
+        className="btn btn-active btn-accent"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        count is {count}
+      </button>
     </>
   );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
+import { ClerkProvider } from "@clerk/clerk-react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+const queryClient = new QueryClient();
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ClerkProvider>
+  </React.StrictMode>,
 );
