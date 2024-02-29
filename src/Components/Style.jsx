c862e28f2c9c28e330ad78473cc7f77d04c0c407
estney <esther.pxy@gmail.com>
2024-02-29 12:21:29 +0800

    new Style jsx

--- src/Components/Style.jsx
+++ src/Components/Style.jsx
@@ -3,7 +3,8 @@ export const multiValue =
   "border border-green rounded-md bg-light-pink p-1 m-0.5";
 
 //Form field box
-export const control = "min-h-12 w-80 mt-4 border-4 border-green rounded-full bg-grey";
+export const control =
+  "min-h-12 w-80 mt-4 border-4 border-green rounded-full bg-grey";
 
 //Dropdown menu when dropdown is selected
 export const menu = "border border-green rounded-full bg-grey ";
@@ -15,7 +16,8 @@ export const option = "hover:bg-light-pink";
 export const center = "flex justify-center items-center";
 
 //Button background light pink
-export const pinkButton = "mb-10 bg-light-pink text-black rounded-full h-10 min-w-20 focus:outline-none focus:ring-2 focus:ring-green-500";
+export const pinkButton =
+  "mb-10 bg-light-pink text-black rounded-full h-10 min-w-20 focus:outline-none focus:ring-2 focus:ring-green-500";
 
 //Light grey icon in Navbar
 export const lgreyIcon = "content-center text-3xl text-grey";
