42917d39d54d0b440e98bf99de6d5de7bb355001
estney <esther.pxy@gmail.com>
2024-03-01 12:05:14 +0800

    social media feed completed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,10 +1,10 @@
+import ActivityCard from "../ActivityCard";
 import FeedHeader from "./FeedHeader";
 
 export default function Feed() {
   return (
     <div>
       <FeedHeader />
-      Main feed page
     </div>
   );
 }
--- src/Components/Feed/FeedHeader.jsx
+++ src/Components/Feed/FeedHeader.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function FeedHeader() {
   return (
-    <div>
-      <div className="flex items-center ">
+    <div className="flex items-center justify-between">
+      <div className="flex items-center">
         <img src="/icons8-tourist-96.png" className="h-16 w-16" />
-        <div className="ml-2 font-semibold">SheTreks</div>
-        <iconify-icon icon="ri:chat-4-line" className="items-end" />
+        <div className="ml-4 text-xl font-semibold">SheTreks</div>
       </div>
+      {/* <Link to="/"> */}
+      <iconify-icon icon="ri:chat-4-line" class="-mt-1 text-3xl text-neutral" />
+      {/* </Link> */}
     </div>
   );
 }
