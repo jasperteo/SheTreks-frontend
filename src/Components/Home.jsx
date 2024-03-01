3750a4cf5339f60324aaeb63d2a9c6c4d29f5835
jasperteo <jaspertzj@outlook.sg>
2024-03-01 17:21:49 +0800

    added sign in and sign up

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,17 @@
+import { SignInButton, SignUpButton } from "@clerk/clerk-react";
+
 export default function Home() {
   return (
     <div>
       <img src="./icons8-tourist-96.png" />
       <div className="text-3xl">SheTreks</div>
+
+      <SignInButton>
+        <button className="btn btn-primary">Sign In</button>
+      </SignInButton>
+      <SignUpButton>
+        <button className="btn btn-primary">Sign Up</button>
+      </SignUpButton>
     </div>
   );
 }
