e1bbbc708b622beeecaf1a3d16f0553eef7d82fc
estney <esther.pxy@gmail.com>
2024-03-01 11:10:30 +0800

    Feature: created component for user small avatar, name and username.

--- src/Components/ActivityCard.jsx
+++ src/Components/ActivityCard.jsx
@@ -1,3 +1,4 @@
+import UserSummProfile from "./UiComponents/UserSummProfile";
 import {
   RoundedAvatar,
   dPinkIcon,
@@ -48,18 +49,7 @@ export default function ActivityCard() {
           </button>
           {/* for oragniser view */}
           <div className="-mt-2 font-semibold">Participants:</div>
-          <div className="flex">
-            <div className="flex-none">
-              <RoundedAvatar
-                image="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                size="8"
-              />
-            </div>
-            <div className="ml-2 mt-1 flex-auto">First Name</div>
-            <div className="ml-2 mt-1 flex-auto font-light italic">
-              @userName
-            </div>
-          </div>
+          <UserSummProfile />
           <button className={`${darkPinkButton} mb-4 text-grey`}>
             <iconify-icon icon="ri:chat-4-line" class={lgreyIcon} />
             DISCUSS PROGRAMME
--- src/Components/RequestCard.jsx
+++ src/Components/RequestCard.jsx
@@ -1,3 +1,4 @@
+import UserSummProfile from "./UiComponents/UserSummProfile";
 import {
   RoundedAvatar,
   brGreenButton,
@@ -12,18 +13,7 @@ export default function RequestCard() {
       <div className="lg:card-sides card mt-8 bg-info shadow-xl">
         <div className="card-body -mb-8">
           <div className="font-semibold">Requests</div>
-          <div className="flex">
-            <div className="flex-none">
-              <RoundedAvatar
-                image="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                size="8"
-              />
-            </div>
-            <div className="ml-2 mt-1 flex-auto">First Name</div>
-            <div className="ml-2 mt-1 flex-auto font-light italic">
-              @userName
-            </div>
-          </div>
+          <UserSummProfile />
           <div>User is requesting to join the acitivty.</div>
           <div className="flex">
             <button className={`${darkPinkButton}  mr-2 mt-2 flex-auto`}>
