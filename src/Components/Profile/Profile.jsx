92433a9cc4256bcf6bbf4efd9a4365099dffe38a
estney <esther.pxy@gmail.com>
2024-03-02 12:55:29 +0800

    Stlye: placement of buttons and included tabs.

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -2,18 +2,24 @@ import { Link } from "react-router-dom";
 import ProfileHeader from "./ProfileHeader";
 import ActivityCard from "../ActivityCard";
 import RequestCard from "../Activity/Individual/RequestCard.jsx";
-import { pinkButton } from "../lib/ClassesName.jsx";
+import { pinkButton, tabColour, activeTabColour } from "../lib/ClassesName.jsx";
+import { useState } from "react";
 
 export default function Profile() {
+  const [activeTab, setActiveTab] = useState(1);
+
+  function updateToggle(id) {
+    setActiveTab(id);
+  }
   return (
     <>
       <ProfileHeader />
       <div className="mt-2 font-semibold">First Name Last Name</div>
       <div className="font-light italic">@userName</div>
       <div>Hello! I am a human.</div>
-      <div className="flex justify-center">
+      <div className="flex justify-start">
         <Link to="/profile/setting">
-          <button className={`${pinkButton} absolute left-8  mt-2`}>
+          <button className={`${pinkButton} mr-8 mt-2`}>
             Edit Profile/Following
           </button>
         </Link>
@@ -22,6 +28,37 @@ export default function Profile() {
         </Link>
       </div>
       <div className="mb-8" />
+      <div>
+        <div role="tablist" className={`${tabColour}`}>
+          <a
+            role="tab"
+            className={`tab ${activeTab === 1 ? `${activeTabColour} tab-active` : ""}`}
+            onClick={() => updateToggle(1)}
+          >
+            CURRENT EVENTS
+          </a>
+          <a
+            role="tab"
+            className={`tab ${activeTab === 2 ? `${activeTabColour} tab-active` : ""}`}
+            onClick={() => updateToggle(2)}
+          >
+            PAST EVENTS
+          </a>
+          <a
+            role="tab"
+            className={`tab ${activeTab === 3 ? `${activeTabColour} tab-active` : ""}`}
+            onClick={() => updateToggle(3)}
+          >
+            PAST ATTENDANCES
+          </a>
+        </div>
+
+        <div className={activeTab == 1 ? "block" : "hidden"}>
+          <p>DIFFERENT</p>
+        </div>
+        <div className={activeTab == 2 ? "block" : "hidden"}></div>
+        <div className={activeTab == 3 ? "block" : "hidden"}></div>
+      </div>
       <ActivityCard />
       <RequestCard />
     </>
