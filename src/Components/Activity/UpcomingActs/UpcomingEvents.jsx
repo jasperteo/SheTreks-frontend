91e6e53306b04cd604c5b42d1a8f5845711e3e0e
estney <esther.pxy@gmail.com>
2024-03-05 09:07:37 +0800

    Feature - pull data for activity in upcoming ord card

--- src/Components/Activity/UpcomingActs/UpcomingEvents.jsx
+++ src/Components/Activity/UpcomingActs/UpcomingEvents.jsx
@@ -2,35 +2,15 @@ import UpcomingOrgActCard from "./UpcomingOrgActCard";
 import UpcomingJoinedActCard from "./UpcomingJoinedActCard";
 import TwoTabs from "../../UiComponents/TwoTabs";
 import { title } from "../../lib/ClassesName";
-import { useQuery } from "@tanstack/react-query";
-import { BACKEND_URL, getRequest } from "../../lib/Constants";
 
 export default function UpcomingEvents() {
-  const currentUserId = 1;
-
-  const upcomingOrgActivity = useQuery({
-    queryKey: [
-      "upcomingOrgActs",
-      `${BACKEND_URL}activities/includeHost/${currentUserId}`,
-    ],
-    queryFn: () =>
-      getRequest(`${BACKEND_URL}activities/includeHost/${currentUserId}`),
-  });
-
-  console.log(upcomingOrgActivity.data);
-
   return (
     <>
       <div className={`${title}`}>SCHEDULE</div>
       <TwoTabs
         leftTitle="ORGANISED"
         rightTitle="JOINED"
-        leftContent={
-          upcomingOrgActivity.data &&
-          upcomingOrgActivity.data.map((post) => (
-            <UpcomingOrgActCard key={post.id} title={post.title} />
-          ))
-        }
+        leftContent={<UpcomingOrgActCard />}
         rightContent={<UpcomingJoinedActCard />}
       />
     </>
