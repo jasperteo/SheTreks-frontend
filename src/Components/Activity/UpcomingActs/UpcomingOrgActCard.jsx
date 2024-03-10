4518298ecd772a97c0e5b9e9200e4f8ef9e39ac7
estney <esther.pxy@gmail.com>
2024-03-10 10:07:38 +0800

    edited

--- src/Components/Activity/UpcomingActs/UpcomingOrgActCard.jsx
+++ src/Components/Activity/UpcomingActs/UpcomingOrgActCard.jsx
@@ -52,7 +52,7 @@ export default function UpcomingOrgActCard() {
               lat: 1.287953,
               lng: 103.851784,
             }}
-            zoom={8}
+            zoom={12}
             mapId="upcoming-map"
           >
             {upcomingOrgActivity.data &&
