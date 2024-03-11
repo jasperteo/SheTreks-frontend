31c9e56c32e3c4577be905dfa741d7abf589792c
estney <esther.pxy@gmail.com>
2024-03-11 17:52:02 +0800

    solve key

--- src/Components/UiComponents/SocialActivityCard.jsx
+++ src/Components/UiComponents/SocialActivityCard.jsx
@@ -13,7 +13,7 @@ import {
 import { useMutation } from "@tanstack/react-query";
 
 export default function SocialActivityCard({ colour, activities, user }) {
-  console.log(activities);
+  // console.log(activities);
   // console.log(user);
 
   const currentUser = useContext(CurrentUserContext);
@@ -92,62 +92,65 @@ export default function SocialActivityCard({ colour, activities, user }) {
               {/* for the current page */}
               {/* show list of paricipants when the length of participants' status = true is more than 1 
               Hide "Participant" status is true length's length is 0*/}
+
               {activity?.participants &&
                 activity?.participants.some(
                   (participant) => participant?.status === true,
                 ) && (
                   <>
-                    <div className="font-semibold">Participants:</div>
+                    <div
+                      className="font-semibold"
+                      key={`${currentUser?.clerkUid}sdasd`}
+                    >
+                      Participants:
+                    </div>
                     {activity?.participants
                       .filter((participant) => participant?.status === true)
                       .map((participant) => (
-                        <>
-                          <UserSummProfile
-                            key={currentUser?.clerkUid}
-                            user={participant}
-                          />
-                        </>
+                        <div key={participant?.id}>
+                          <UserSummProfile user={participant} />
+                        </div>
                       ))}
                   </>
                 )}
-              {/* When I view another person's account, 
+            </div>
+            {/* When I view another person's account, 
               //the card should show the join button if i am not the host, participant.
               //button should not appear in my profile when i view my profile.
               //the button should only be available in current section (future events) */}
-              {activity?.hostId !== currentUser?.id &&
-              !activity?.participants?.some(
-                (participant) => participant?.user.id === currentUser?.id,
-              ) &&
-              new Date(activity?.eventDate) > new Date() ? (
-                <div className="card-body -mb-4">
-                  {/* //Request was previosuly sent, but not approved or declined. Do not allow user to submit another request. */}
-                  {activity?.participants?.some((participant) => {
-                    return (
-                      participant?.user.id === currentUser?.id &&
-                      participant?.status === false
-                    );
-                  }) ? null : (
-                    <>
-                      {requestSent ? (
-                        <div className="text-center text-neutral">
-                          REQUEST SENT!
-                        </div>
-                      ) : (
-                        <button
-                          className={`${darkPinkButton} -mb-4 -mt-4 text-grey`}
-                          onClick={() => {
-                            handleClick(activity.id);
-                            setRequestSent(true);
-                          }}
-                        >
-                          JOIN NOW
-                        </button>
-                      )}
-                    </>
-                  )}
-                </div>
-              ) : null}
-            </div>
+            {activity?.hostId !== currentUser?.id &&
+            !activity?.participants?.some(
+              (participant) => participant?.user.id === currentUser?.id,
+            ) &&
+            new Date(activity?.eventDate) > new Date() ? (
+              <div className="card-body -mb-4">
+                {/* //Request was previosuly sent, but not approved or declined. Do not allow user to submit another request. */}
+                {activity?.participants?.some((participant) => {
+                  return (
+                    participant?.user.id === currentUser?.id &&
+                    participant?.status === false
+                  );
+                }) ? null : (
+                  <>
+                    {requestSent ? (
+                      <div className="text-center text-neutral">
+                        REQUEST SENT!
+                      </div>
+                    ) : (
+                      <button
+                        className={`${darkPinkButton} -mb-4 -mt-4 text-grey`}
+                        onClick={() => {
+                          handleClick(activity.id);
+                          setRequestSent(true);
+                        }}
+                      >
+                        JOIN NOW
+                      </button>
+                    )}
+                  </>
+                )}
+              </div>
+            ) : null}
           </div>
         ))}
     </div>
