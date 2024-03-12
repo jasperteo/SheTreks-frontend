b615080c01f646553ef000ccac6cc63f4f0cf392
estney <esther.pxy@gmail.com>
2024-03-12 12:10:43 +0800

    create feed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,31 +1,165 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import FeedHeader from "./FeedHeader";
+import {
+  BACKEND_URL,
+  CurrentUserContext,
+  categoryIcon,
+  formatDateMaskedTime,
+  formatDateandTime,
+  getRequest,
+  postRequest,
+} from "../lib/Constants";
+import { useContext } from "react";
+import SocialActivityCard from "../UiComponents/SocialActivityCard";
+import RoundedAvatar from "../UiComponents/RoundedAvatar";
+import { dPinkIcon, darkPinkButton } from "../lib/ClassesName";
+import UserSummProfile from "../UiComponents/UserSummProfile";
 
 export default function Feed() {
-  //extract all "to follow" id - user id
-  //map into all getAllCurrent
-  //pass into prop
+  const currentUser = useContext(CurrentUserContext);
+  const queryClient = useQueryClient();
+
+  const feedActivities = useQuery({
+    queryKey: [
+      "feedActivitieis",
+      `${BACKEND_URL}/activities/current/${currentUser?.id}/`,
+    ],
+    queryFn: () =>
+      getRequest(`${BACKEND_URL}/activities/current/${currentUser?.id}`),
+    enabled: !!currentUser?.id,
+  });
+
+  console.log(feedActivities.data);
+
+  const { mutate: mutateJoin } = useMutation({
+    mutationFn: (data) =>
+      postRequest(
+        `${BACKEND_URL}/activities/${data.activityId}/participants`,
+        data,
+      ),
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: [
+          "currentActivities",
+          `${BACKEND_URL}/activities/current/${user?.id}/`,
+        ],
+      }),
+  });
+
+  const handleClick = (activityId) =>
+    mutateJoin({ activityId, userId: currentUser.id });
 
   return (
     <>
       <FeedHeader />
-      {/* To display a list of events joined and organized by the account owner's followings. */}
-      {/* <ActivityCard
-        accOwnerImage="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-        accOwnerUserName="Fiona"
-        accOwnerStatus="(Attendee)"
-        city="Hanoi"
-        country="Vietnam"
-        activityTitle="Fly Fly"
-        date="23 Jan 2023"
-        time="08:00AM"
-        activityDescription="Feel like a garbage bag!"
-        organiserImageURL="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-        organiserFirstName="Tay Tay"
-        organiserUsername="swiftieeee"
-        activityImageURL="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
-        categoryApiId={1}
-        catergoryName="Fooddd"
-      /> */}
+      <div>
+        {feedActivities.data?.map((activity) => (
+          <div
+            className={`lg:card-sides card mt-8 bg-primary shadow-xl`}
+            key={activity.id}
+          >
+            <div className="card-body">
+              <div className="flex">
+                <div className="flex-none">
+                  <RoundedAvatar image={activity?.user?.imageUrl} size="8" />
+                </div>
+                {/* to indicidate if user is an attendee */}
+                <div className="ml-2 mt-1 flex-auto font-light italic">
+                  {`@${activity?.user?.username}`}
+                  {activity?.hostId !== currentUser?.id
+                    ? " (Attendee)"
+                    : " (Organiser)"}
+                </div>
+              </div>
+              <div className="font-semibold">
+                {activity?.location?.city}, {activity?.location?.country}
+              </div>
+
+              <div className="font-semibold">{activity?.title}</div>
+
+              <div className="font-light italic">
+                {new Date(activity?.eventDate) < new Date()
+                  ? formatDateandTime(activity?.eventDate) // past events
+                  : activity?.participants?.some(
+                        (participant) =>
+                          participant?.user.id === currentUser?.id &&
+                          participant?.status === true,
+                      )
+                    ? formatDateandTime(activity?.eventDate)
+                    : formatDateMaskedTime(activity?.eventDate)}
+              </div>
+              <div>{activity?.description}</div>
+              <div className="items-left flex flex-col flex-wrap space-x-1 ">
+                {activity.categories.map((category) => (
+                  <div className={`${dPinkIcon}`} key={category?.id}>
+                    <iconify-icon
+                      inline
+                      icon={`${categoryIcon(category?.id)}`}
+                    />
+                    <p className="text-xs">{category?.categoryName}</p>
+                  </div>
+                ))}
+              </div>
+              {/* remove organiser section if user is the organiser */}
+
+              {activity?.hostId !== currentUser?.id && (
+                <>
+                  <div className="font-semibold">Organiser:</div>
+                  <UserSummProfile user={activity} key={activity?.user?.id} />
+                </>
+              )}
+              {/* for the current page */}
+              {/* show list of paricipants when the length of participants' status = true is more than 1 */}
+              <>
+                <div className="font-semibold">Participants:</div>
+                {activity?.participants?.map(
+                  (participant) =>
+                    !!participant?.status && (
+                      <UserSummProfile
+                        user={participant}
+                        key={participant?.id}
+                      />
+                    ),
+                )}
+              </>
+              {/* When I view another person's account, 
+              //the card should show the join button if i am not the host, participant.
+              //button should not appear in my profile when i view my profile.
+              //the button should only be available in current section (future events) */}
+              {activity?.hostId !== currentUser?.id &&
+                new Date(activity?.eventDate) > new Date() && (
+                  <div className="card-body -mb-4">
+                    {activity?.participants?.some(
+                      (participant) =>
+                        participant?.user.id === currentUser?.id &&
+                        !participant?.status,
+                    ) ? (
+                      <button
+                        className={`${darkPinkButton} -mb-4 -mt-4 text-grey`}
+                        disabled
+                      >
+                        REQUESTED
+                      </button>
+                    ) : (
+                      activity?.participants?.some(
+                        (participant) =>
+                          participant?.user.id === currentUser?.id &&
+                          participant?.status,
+                      ) || (
+                        <button
+                          className={`${darkPinkButton} -mb-4 -mt-4 text-grey`}
+                          onClick={() => handleClick(activity.id)}
+                        >
+                          JOIN NOW
+                        </button>
+                      )
+                    )}
+                  </div>
+                )}
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
---
+++ src/Components/Feed/FeedCard.jsx
@@ -0,0 +1,3 @@
+export default function FeedCard() {
+  return <div></div>;
+}
