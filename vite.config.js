e99cf18bd5493d52d063ac4563355dc9369cad4f
jasperteo <jaspertzj@outlook.sg>
2024-03-21 02:22:51 +0800

    added million.js for optimal rendering

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
+import million from "million/compiler";
+import MillionCompiler from "@million/lint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    million.vite({ auto: true }),
+    MillionCompiler.vite(),
     VitePWA({
       registerType: "autoUpdate",
       devOptions: { enabled: true },
